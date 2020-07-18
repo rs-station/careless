ee23efb53bd4bc81244e02ba9414a09515f5eacd
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-07-18 16:38:51 -0400

    move models.distributions to models.priors

---
+++ careless/models/priors/base.py
@@ -0,0 +1,9 @@
+
+class Prior():
+    """Base class for prior distributions on merged normalized structure factor amplitudes."""
+    def log_prob(self):
+        raise NotImplementedError("No log_prob method defined. All Priors must implement a log_prob method")
+
+    def prob(self):
+        raise NotImplementedError("No prob method defined. All Priors must implement a prob method")
+
