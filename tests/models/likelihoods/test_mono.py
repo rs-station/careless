9219de13c9ed0f25fc206146a7640645df3259b9
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-07-19 00:18:20 -0400

    mono likelihood test

---
+++ tests/models/likelihoods/test_mono.py
@@ -0,0 +1,21 @@
+from careless.models.likelihoods.mono import *
+
+iobs,sigiobs = np.random.random((2, 100)).astype(np.float32)
+samples = np.random.random(100).astype(np.float32)
+
+def test_NormalLikelihood():
+    likelihood = NormalLikelihood(iobs, sigiobs)
+    likelihood.prob(samples)
+    likelihood.log_prob(samples)
+
+def test_LaplaceLikelihood():
+    likelihood = LaplaceLikelihood(iobs, sigiobs)
+    likelihood.prob(samples)
+    likelihood.log_prob(samples)
+
+def test_StudentTLikelihood():
+    dof = 4.
+    likelihood = StudentTLikelihood(iobs, sigiobs, dof)
+    likelihood.prob(samples)
+    likelihood.log_prob(samples)
+
