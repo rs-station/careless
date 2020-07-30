f81aab0388bac2f85b0b50a8d06492c99a2d8c44
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-07-30 10:01:08 -0400

    really reinstall everything

--- tests/test_git_current.sh
+++ tests/test_git_current.sh
@@ -6,10 +6,9 @@ eval "$(conda shell.bash hook)"
 
 conda create -yn $NAME python=$PYTHON_VERSION
 conda activate $NAME
-conda env list
 git clone ssh://git@$URL
 cd $NAME
-pip install -e .
+pip install --no-cache-dir -e .
 python setup.py test
 
 
