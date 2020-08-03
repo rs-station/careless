80d38f848edc56b03356a5f5c505765854be898e
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-08-03 17:43:26 -0400

    adding a merging script to laue sample data

---
+++ data/laue/pyp/merge.sh
@@ -0,0 +1,9 @@
+careless poly \
+  --merge-files=False \
+  --iterations=10000 \
+  --wavelength-key='Wavelength' \
+  "X,Y,Wavelength,Hobs,Kobs,Lobs,image_id" \
+  off_varEll.mtz \
+  2ms_varEll.mtz \
+  merge/pyp
+
