82a5d14f3fc9495b4ce589eca0cfda499236efb1
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-08-04 09:19:23 -0400

    skip ci

--- data/laue/pyp/merge.sh
+++ data/laue/pyp/merge.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+#
+#
+#SBATCH -p seas_gpu_requeue # partition (queue)
+#SBATCH --mem 12G # memory pool for all cores
+#SBATCH -t 0-0:45 # time (D-HH:MM)
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=v100
+
 mkdir merge
 careless poly \
   --merge-files=False \
-  --iterations=10000 \
+  --iterations=30000 \
+  --learning-rate=0.001 \
   --wavelength-key='Wavelength' \
   "X,Y,Wavelength,Hobs,Kobs,Lobs,image_id" \
   off_varEll.mtz \
