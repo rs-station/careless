b458eecfef227c322cb90a8964bd77d78f80761c
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-08-28 09:34:58 -0400

    skip ci

--- doc/figures/pgm_v1.py
+++ doc/figures/pgm_v1.py
@@ -8,27 +8,28 @@ p = daft.PGM()
 
 d = 4
 layers = 5
-
+nn_scale = 2.
+nn_arrows = False
 
 for i in range(layers):
     for j in range(d):
-        p.add_node(f'{i}_{j}', x=i, y=j, fixed=True)
+        p.add_node(f'{i}_{j}', x=i, y=j, fixed=True, scale=nn_scale)
     if i > 0:
         for j in range(d):
             for k in range(d):
-                p.add_edge(f'{i}_{j}', f'{i-1}_{k}', directed=False)
+                p.add_edge(f'{i-1}_{k}', f'{i}_{j}', directed=nn_arrows)
 
 for i in range(d):
     p.add_node(f'm_{i}', r'$m_{i}$', x=-1., y=i, observed=True)
-    p.add_edge(f"m_{i}", f"{0}_{i}")
+    p.add_edge(f"m_{i}", f"{0}_{i}", directed=nn_arrows)
 
 
-p.add_node(f'{layers}_0', x=layers, y=d/2, fixed=True)
-p.add_node(f'{layers}_1', x=layers, y=d/2-1.0, fixed=True)
+p.add_node(f'{layers}_0', '$\mu$', x=layers, y=d/2, fixed=True, scale=2.*nn_scale, label_params={'color': 'w'}, offset=(0., -6.))
+p.add_node(f'{layers}_1', '$\sigma$', x=layers, y=d/2-1.0, fixed=True, scale=2.*nn_scale, label_params={'color': 'w'}, offset=(0., -7.))
 
 for i in range(d):
-    p.add_edge(f"{layers}_0", f"{layers-1}_{i}", directed=False)
-    p.add_edge(f"{layers}_1", f"{layers-1}_{i}", directed=False)
+    p.add_edge(f"{layers-1}_{i}", f"{layers}_0", directed=nn_arrows)
+    p.add_edge(f"{layers-1}_{i}", f"{layers}_1", directed=nn_arrows)
 
 p.add_node("Sigma", r"$\Sigma$", x = layers + 1, y = (d-1)/2.)
 p.add_edge(f"{layers}_0", "Sigma")
