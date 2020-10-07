a5bc55beb5aead839c6e2e8fae90772f6b6d20bd
Kevin Dalton <kevinmdalton@gmail.com>
2020-10-07 18:21:58 -0400

    maybe a quasi proper posterior parameterization

---
+++ careless/models/merging/surrogate_posteriors.py
@@ -0,0 +1,45 @@
+from careless.utils.distributions import Rice,FoldedNormal
+from tensorflow_probability import distributions as tfd
+from tensorflow_probability.python.internal import tensor_util
+import tensorflow as tf
+import numpy as np
+
+class RiceWoolfson(tfd.Distribution):
+    def __init__(self, loc, scale, centric):
+        """
+        This is a hybrid distribution to parameterize posteriors over structure factors. 
+        It uses the Rice distribution to model acentric structure factors and
+        the Folded normal or "Woolfson" distribution to model centrics. 
+
+        Parameters
+        ----------
+        loc : array (float)
+            location parameter for the distributions
+        scale : array (float)
+            scale parameter for the distributions
+        centric : array (float;bool)
+            Array that same length as loc and scale that is 1./True for centric reflections and 0./False
+        """
+        self._loc   = tensor_util.convert_nonref_to_tensor(loc, dtype=tf.float32)
+        self._scale = tensor_util.convert_nonref_to_tensor(scale, dtype=tf.float32)
+        self._centric = np.array(centric, dtype=np.bool)
+        self._woolfson = FoldedNormal(self._loc, self._scale)
+        self._rice = Rice(self._loc, self._scale)
+
+    def mean(self):
+        return tf.where(self._centric, self._woolfson.mean(), self._rice.mean())
+
+    def variance(self):
+        return tf.where(self._centric, self._woolfson.variance(), self._rice.variance())
+
+    def stddev(self):
+        return tf.where(self._centric, self._woolfson.stddev(), self._rice.stddev())
+
+    def sample(self):
+        return tf.where(self._centric, self._woolfson.sample(), self._rice.sample())
+
+    def log_prob(self, x):
+        return tf.where(self._centric, self._woolfson.log_prob(x), self._rice.log_prob(x))
+
+    def prob(self, x):
+        return tf.where(self._centric, self._woolfson.prob(x), self._rice.prob(x))
