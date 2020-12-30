92a78c16024fd720f38f3312749df58bd8653dfa
Kevin Dalton <kevinmdalton@gmail.com>
2020-12-29 22:03:28 -0500

    more better dependency resolution

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@ setup(
     install_requires=[
         "reciprocalspaceship>=0.9.1",
         "tqdm",
-        "tensorflow==2.4",
+        "tensorflow>=2.4",
         "tensorflow-probability",
-        "numpy==1.19.2",
     ],
     scripts = [
             'careless/careless',
