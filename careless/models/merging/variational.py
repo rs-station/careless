55eb9d84b5acc784360abce26ea7e463a3ad684f
Kevin Dalton <kevinmdalton@gmail.com>
2021-08-19 18:23:50 -0400

    variationalmergingmodel rewrite wip

--- careless/models/merging/variational.py
+++ careless/models/merging/variational.py
@@ -1,13 +1,99 @@
-from careless.models.base import PerGroupModel
+from careless.models.base import BaseModel
 from careless.utils.shame import sanitize_tensor
 from careless.models.merging.surrogate_posteriors import TruncatedNormal
 from tqdm.autonotebook import tqdm
 import tensorflow_probability as tfp
 import tensorflow as tf
+from tensorflow import keras as tfk
 import numpy as np
 
 
-class VariationalMergingModel(PerGroupModel, tf.Module):
+class VariationalMergingModel(BaseModel):
+    """
+    Merge data with a posterior parameterized by a surrogate distribution.
+    """
+    def __init__(self, surrogate_posterior, prior, likelihood, scaling_model):
+        """"
+        Parameters
+        ----------
+        surrogate_posterior : tfd.Distribution
+            A surrogate posterior distribution to use. 
+            If non is supplied, the default truncated normal distribution will be used. 
+            Any posteriors passed in with this arg must have all properly transformed parameters in their
+            `self.trainable_variables` iterable.  Use `tfp.util.TransformedVariable` to ensure positivity constraints
+            where applicable.
+        prior : distribution
+            Prior distribution on merged, normalized structure factor amplitudes. 
+            Either a Distribution from tensorflow_probability.distributions or 
+            a Prior from careless.models.distributions. This must implement .log_prob. 
+            This distribution must have an `event_shape` equal to `np.max(miller_ids) + 1`.
+        likelihood : careless.models.likelihood.Likelihood
+            This is a Likelihood object from careless.
+        scaling_model : careless.models.base.BaseModel
+            An instance of a class from carless.model.scaling 
+        """
+        super.__init__(self)
+        self.prior = prior
+        self.surrogate_posterior = surrogate_posterior
+        self.likelihood = likelihood
+        self.scaling_model = scaling_model
+
+        #This is just to make sure tf picks up the variables which doesn't always happen with
+        #transformed variables
+        self.qvars = surrogate_posterior.variables
+
+    def expectation(self, inputs):
+        """
+        Compute the expected value of reflection observations under the current model.
+        """
+        scale_dist = self.scaling_model(inputs)
+
+        #This is <F**2.>
+        f2 = self.surrogate_posterior.mean()**2. + self.surrogate_posterior.stddev()**2.
+
+        refl_id = self.get_refl_id(inputs)
+        ipred = scale_dist.mean() * tf.gather(f2, refl_id, axis=-1)
+        return ipred
+
+    def call(self, inputs, mc_samples=1):
+        """
+        Parameters
+        ----------
+        inputs : data
+            inputs is a data structure like [refl_id, image_id, metadata, intensity, uncertainty]. 
+            This can be a tf.DataSet, or a group of tensors. 
+
+        Returns
+        -------
+        predictions : tf.Tensor
+            Values predicted by the model for this sample. 
+        """
+        z_f = self.surrogate_posterior.sample(mc_samples)
+
+        scale_dist = self.scaling_model(inputs)
+        z_scale = scale_dist.sample(mc_samples)
+
+        kl_div = tf.reduce_sum(self.surrogate_posterior.log_prob(z_f) - self.prior.log_prob(z_f))
+
+        refl_id = self.get_refl_id(inputs)
+        ipred = z_scale * tf.gather(z_f, refl_id, axis=-1)**2.
+
+        likelihood = self.likelihood(inputs)
+        ll = tf.reduce_sum(likelihood.log_prob(ipred))
+
+        #just to make things consistent across mc_samples
+        ll     /= mc_samples
+        kl_div /= mc_samples
+
+        #Do some keras-y stuff
+        self.add_loss(-ll)
+        self.add_loss(kl_div)
+        self.add_metric(-ll, name="NLL")
+        self.add_metric(kl_div, name="KLDiv")
+        return ipred
+
+#Legacy class to be deleted....
+class VariationalXXMergingModel(PerGroupModel, tf.Module):
     """
     Merge data with a posterior parameterized by a surrogate distribution.
     """
