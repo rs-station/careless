dfd963b4fdfc48ca81048a4ef2cc2b1fb89c345a
Kevin Dalton <kevinmdalton@gmail.com>
2021-08-19 18:20:06 -0400

    added reciprocal asu classes

---
+++ careless/io/asu.py
@@ -0,0 +1,125 @@
+import reciprocalspaceship as rs
+import numpy as np
+
+class ReciprocalASU():
+    def __init__(self, cell, spacegroup, dmin, anomalous):
+        """
+        Parameters
+        ----------
+        cell : gemmi.UnitCell
+            A gemmi.UnitCell instance
+        spacegroup : gemmi.SpaceGroup
+            A gemmi.SpaceGroup instance
+        dmin : float
+            The maximum resolution in Angstroms
+        anomalous : bool
+            Whether the ASU includes Friedel minus acentric reflections
+        """
+        self.cell = cell
+        self.spacegroup = spacegroup
+        self.dmin = dmin
+        self.anomalous = anomalous
+        self.Hall = rs.utils.generate_reciprocal_asu(
+                self.cell, 
+                self.spacegroup, 
+                self.dmin, 
+                self.anomalous
+            )
+        h,k,l = self.Hall.T
+        lookup_table = rs.DataSet({
+            'H' : h,
+            'K' : k,
+            'L' : l,
+            'id' : np.arange(len(h))
+            }, 
+            cell=cell,
+            spacegroup=spacegroup,
+        )
+        self.lookup_table = lookup_table
+
+    def to_refl_id(self, H):
+        """
+        Parameters
+        ----------
+        H : np.array
+            (n x 3) array of miller indices to convert to integer reflection ids
+
+        Returns
+        -------
+        refl_id : np.array
+            (n x 1) array of integer reflection ids
+        """
+        return self.lookup_table.set_index(['H','K','L']).loc[H.tolist(), 'id']
+
+    def to_miller_index(self, refl_id):
+        """
+        Parameters
+        ----------
+        refl_id : np.array
+            (n x 1) array of integer reflection ids to convert to miller indices
+
+        Returns
+        -------
+        H : np.array
+            (n x 3) array of miller indices 
+        """
+        return self.lookup_table.set_index('id').loc[refl_id].get_hkls()
+
+class ReciprocalASUCollection():
+    def __init__(self, reciprocal_asus):
+        """
+        Parameters
+        ----------
+        reciprocal_asus : list
+            A list of ReciprocalAsu instances
+        """
+        self.reciprocal_asus = reciprocal_asus
+        self.lookup_table = None
+        for asu_id,asu in enumerate(self.reciprocal_asus):
+            tab = asu.lookup_table.copy()
+            tab['asu_id'] = asu_id
+            if self.lookup_table is not None:
+                tab['id'] += len(self.lookup_table)
+                self.lookup_table = rs.concat((self.lookup_table, tab), check_isomorphous=False)
+            else:
+                self.lookup_table =  tab
+        self.asu_and_miller_lookup_table = self.lookup_table.set_index(['asu_id', 'H', 'K', 'L'])
+        self.refl_id_lookup_table = self.lookup_table.set_index('id')
+
+    def to_asu_id_and_miller_index(self, refl_id):
+        """
+        Parameters
+        ----------
+        refl_id : np.array
+            (n x 1) array of integer reflection ids to convert to miller indices
+
+        Returns
+        -------
+        asu_id : np.array
+            (n x 1) array of asu ids 
+        H : np.array
+            (n x 3) array of miller indices 
+        """
+        data = self.refl_id_lookup_table.loc[refl_id]
+        H = data.get_hkls()
+        asu_id = data['asu_id'].to_numpy('int')
+        return asu_id, H
+
+    def to_refl_id(self, asu_id, H):
+        """
+        Parameters
+        ----------
+        asu_id : np.array
+            (n x 1) array of asu ids 
+        H : np.array
+            (n x 3) array of miller indices 
+
+        Returns
+        -------
+        refl_id : np.array
+            (n x 1) array of integer reflection ids to convert to miller indices
+        """
+        index = np.concatenate((asu_id, H), axis=1).astype('int')
+        return self.asu_and_miller_lookup_table.loc[index.tolist(), 'id'].to_numpy('int')
+
+
---
+++ tests/io/test_asu.py
@@ -0,0 +1,50 @@
+from careless.io.asu import ReciprocalASU,ReciprocalASUCollection
+import pytest
+import numpy as np
+import reciprocalspaceship as rs
+import gemmi
+
+
+@pytest.mark.parametrize('anomalous', [True, False])
+@pytest.mark.parametrize('dmin', [10., 5.])
+def test_reciprocal_asu(dmin, anomalous, cell_and_spacegroups):
+    for cell,sg in cell_and_spacegroups:
+        rasu = ReciprocalASU(cell, sg, dmin, anomalous)
+        Hall = rs.utils.generate_reciprocal_asu(
+            cell,
+            sg,
+            dmin,
+            anomalous
+        )
+        d_hkl = cell.calculate_d_array(rasu.lookup_table.get_hkls())
+        assert np.all(d_hkl >= dmin)
+
+        refl_id = rasu.to_refl_id(Hall)
+        assert np.all(refl_id == np.arange(len(Hall)))
+
+        miller_index = rasu.to_miller_index(np.arange(len(Hall)))
+        assert np.all(miller_index == Hall)
+
+
+@pytest.mark.parametrize('anomalous', [[True, True], [True, False], [False, False]])
+@pytest.mark.parametrize('dmin', [[10., 10.], [5., 10.], [5., 5.]])
+def test_double_reciprocal_asu_collection(dmin, anomalous, cell_and_spacegroups):
+    for cell,sg in cell_and_spacegroups:
+        rasus = [ReciprocalASU(cell, sg, d, a) for d,a in zip(dmin,anomalous)]
+        rac = ReciprocalASUCollection(rasus)
+
+        Hall_per_asu = [rs.utils.generate_reciprocal_asu(cell,sg,d,a) for d,a in zip(dmin,anomalous)]
+        n = np.sum([len(i) for i in Hall_per_asu])
+
+        assert len(rac.lookup_table) == n
+        refl_ids = []
+        for asu_id,h in enumerate(Hall_per_asu):
+            refl_id = rac.to_refl_id(asu_id*np.ones((len(h), 1)), h)
+            asu_id_test,h_test = rac.to_asu_id_and_miller_index(refl_id)
+            assert np.all(asu_id == asu_id_test)
+            assert np.all(h == h_test)
+            refl_ids.append(refl_id.flatten())
+
+        refl_ids = np.concatenate(refl_ids)
+        assert len(refl_ids) == len(np.unique(refl_ids))
+
