7878f581b916ed20072619f4790c2700d33292e9
Kevin Dalton <kevinmdalton@gmail.com>
2021-08-20 10:50:13 -0400

    got empirical priors working and tested

--- careless/models/priors/empirical.py
+++ careless/models/priors/empirical.py
@@ -1,50 +1,83 @@
+import tensorflow as tf
 from tensorflow_probability import distributions as tfd
 import numpy as np
 from careless.models.priors.base import Prior
 from careless.models.merging.surrogate_posteriors import RiceWoolfson
 
 
-class LaplaceReferencePrior(tfd.Laplace, Prior):
+class ReferencePrior():
+    """
+    A Prior class with a `log_prob` implementation that returns zeros for unobserved miller indices.
+     - This class is not meant to be used directly. 
+     - Extensions of this class must set the `base_dist` attribute with a tfd.Distribution or similar object with
+       a `.log_prob(values)` method.
+    """
+    base_dist = None
+    def __init__(self, observed=None):
+        if observed is None:
+            self.idx = None
+        else:
+            idx = tf.where(observed)
+            self.idx = tf.reshape(idx, (-1,))
+
+
+    def log_prob(self, values):
+        if self.idx is None:
+            return self.base_dist.log_prob(values)
+        obs = tf.gather(values, self.idx, axis=-1)
+        log_prob = self.base_dist.log_prob(obs)
+
+        return tf.transpose(tf.scatter_nd(
+            self.idx[...,None], 
+            tf.transpose(log_prob), 
+            values.shape[::-1]
+        ))
+
+class LaplaceReferencePrior(ReferencePrior):
     """
     A Laplacian prior distribution centered at empirical structure factor amplitudes derived from a conventional experiment.
     """
-    def __init__(self, Fobs, SigFobs):
+    def __init__(self, Fobs, SigFobs, observed=None):
         """
         Parameters
         ----------
         Fobs : array
             numpy array or tf.Tensor containing observed structure factors amplitudes from a reference structure.
-        SigFobs
+        SigFobs : array
             numpy array or tf.Tensor containing error estimates for structure factors amplitudes from a reference structure.
+        observed : array (optional)
+            boolean numpy array or tf.Tensor which has True for all observed miller indices.
         """
+        super().__init__(observed)
         loc = np.array(Fobs, dtype=np.float32)
         scale = np.array(SigFobs, dtype=np.float32)/np.sqrt(2.)
-        super().__init__(loc, scale)
+        self.base_dist = tfd.Laplace(loc, scale)
 
-
-class NormalReferencePrior(tfd.Normal, Prior):
+class NormalReferencePrior(ReferencePrior):
     """
     A Normal prior distribution centered at empirical structure factor amplitudes derived from a conventional experiment.
     """
-    def __init__(self, Fobs, SigFobs):
+    def __init__(self, Fobs, SigFobs, observed=None):
         """
         Parameters
         ----------
         Fobs : array
             numpy array or tf.Tensor containing observed structure factors amplitudes from a reference structure.
-        SigFobs
+        SigFobs : array
             numpy array or tf.Tensor containing error estimates for structure factors amplitudes from a reference structure.
+        observed : array (optional)
+            boolean numpy array or tf.Tensor which has True for all observed miller indices.
         """
+        super().__init__(observed)
         loc = np.array(Fobs, dtype=np.float32)
         scale = np.array(SigFobs, dtype=np.float32)
-        super().__init__(loc, scale)
+        self.base_dist = tfd.Normal(loc, scale)
 
-
-class StudentTReferencePrior(tfd.StudentT, Prior):
+class StudentTReferencePrior(ReferencePrior):
     """
     A Student's T prior distribution centered at empirical structure factor amplitudes derived from a conventional experiment.
     """
-    def __init__(self, Fobs, SigFobs, dof):
+    def __init__(self, Fobs, SigFobs, dof, observed=None):
         """
         Parameters
         ----------
@@ -53,13 +86,35 @@ class StudentTReferencePrior(tfd.StudentT, Prior):
         SigFobs : array
             numpy array or tf.Tensor containing error estimates for structure factors amplitudes from a reference structure.
         dof : float
-            Degrees of freedom for the Student T distribution.
+            degrees of freedom for the student's t distribution.
+        observed : array (optional)
+            boolean numpy array or tf.Tensor which has True for all observed miller indices.
         """
+        super().__init__(observed)
         loc = np.array(Fobs, dtype=np.float32)
         scale = np.array(SigFobs, dtype=np.float32)
-        super().__init__(dof, loc, scale)
+        self.base_dist = tfd.StudentT(dof, loc, scale)
 
-class RiceWoolfsonReferencePrior(RiceWoolfson, Prior):
+class RiceWoolfsonReferencePrior(ReferencePrior):
     """
-    Just an alias to the Rice Woolfson surrogate posterior.
+    A Rice Woolfson prior distribution centered at empirical structure factor amplitudes derived from a conventional experiment.
     """
+    def __init__(self, Fobs, SigFobs, centric, observed=None):
+        """
+        Parameters
+        ----------
+        Fobs : array
+            numpy array or tf.Tensor containing observed structure factors amplitudes from a reference structure.
+        SigFobs : array
+            numpy array or tf.Tensor containing error estimates for structure factors amplitudes from a reference structure.
+        centric : array
+            boolean numpy array or tf.Tensor which has True for all centric reflections.
+        observed : array (optional)
+            boolean numpy array or tf.Tensor which has True for all observed miller indices.
+        """
+        super().__init__(observed)
+        loc = np.array(Fobs, dtype=np.float32)
+        scale = np.array(SigFobs, dtype=np.float32)
+        self.base_dist = RiceWoolfson(loc, scale, centric)
+
+
--- tests/models/priors/test_empirical.py
+++ tests/models/priors/test_empirical.py
@@ -1,3 +1,8 @@
+import pytest
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow_probability import distributions as tfd
+from careless.models.merging.surrogate_posteriors import RiceWoolfson
 from careless.models.priors.empirical import *
 import numpy as np
 
@@ -6,33 +11,75 @@ status = disable_gpu()
 assert status
 
 
+observed = np.random.choice([True, False], 100)
+observed[0] = True  #just in case
+observed[1] = False #just in case
 Fobs,SigFobs = np.random.random((2, 100)).astype(np.float32)
+Fobs[~observed] = 1.
+SigFobs[~observed] = 1.
 
+def ReferencePrior_test(p, ref, mc_samples):
+    #This part checks indexing and gradient numerics
+    q = tfd.TruncatedNormal( #<-- use this dist because RW has positive support
+        Fobs, 
+        tfp.util.TransformedVariable( 
+            SigFobs,
+            tfp.bijectors.Softplus(),
+        ),
+        low=1e-5,
+        high=1e10,
+    )
+    with tf.GradientTape() as tape:
+        z = q.sample(mc_samples)
+        log_probs = p.log_prob(z)
+    grads = tape.gradient(log_probs, q.trainable_variables)
 
-def test_LaplaceReferencePrior():
-    p_E = LaplaceReferencePrior(Fobs, SigFobs)
-    mean = p_E.mean()
-    std = p_E.stddev()
-    samples = np.random.normal(Fobs, SigFobs)
-    probs = p_E.log_prob(samples)
-    log_probs = p_E.log_prob(samples)
-    assert np.all(np.isfinite(probs))
     assert np.all(np.isfinite(log_probs))
-    assert np.all(np.isfinite(mean))
-    assert np.all(np.isfinite(std))
-    assert len(mean) == len(Fobs)
-    assert len(std) == len(Fobs)
+    for grad in grads:
+        assert np.all(np.isfinite(grad))
+    assert np.all(log_probs.numpy()[...,~observed] == 0.)
 
-def test_NormalReferencePrior():
-    p_E = NormalReferencePrior(Fobs, SigFobs)
-    mean = p_E.mean()
-    std = p_E.stddev()
-    samples = np.random.normal(Fobs, SigFobs)
-    probs = p_E.log_prob(samples)
-    log_probs = p_E.log_prob(samples)
-    assert np.all(np.isfinite(probs))
-    assert np.all(np.isfinite(log_probs))
-    assert np.all(np.isfinite(mean))
-    assert np.all(np.isfinite(std))
-    assert len(mean) == len(Fobs)
-    assert len(std) == len(Fobs)
+    #This tests that the observed values follow the correct distribution
+    z = ref.sample(mc_samples)
+    expected = ref.log_prob(z).numpy()[...,observed]
+    result = p.log_prob(z).numpy()[...,observed]
+    assert np.allclose(expected, result)
+
+@pytest.mark.parametrize('mc_samples', [3, 1])
+def test_LaplaceReferencePrior(mc_samples):
+    p = LaplaceReferencePrior(Fobs[observed], SigFobs[observed], observed)
+    q = tfd.Laplace(Fobs, SigFobs/np.sqrt(2.))
+    ReferencePrior_test(p, q, mc_samples)
+
+
+@pytest.mark.parametrize('mc_samples', [3, 1])
+def test_NormalReferencePrior(mc_samples):
+    p = NormalReferencePrior(Fobs[observed], SigFobs[observed], observed)
+    q = tfd.Normal(Fobs, SigFobs)
+    ReferencePrior_test(p, q, mc_samples)
+
+
+@pytest.mark.parametrize('mc_samples', [3, 1])
+def test_StudentTReferencePrior(mc_samples):
+    p = StudentTReferencePrior(Fobs[observed], SigFobs[observed], 4., observed)
+    q = tfd.StudentT(4, Fobs, SigFobs)
+    ReferencePrior_test(p, q, mc_samples)
+
+
+@pytest.mark.parametrize('mc_samples', [3, 1])
+@pytest.mark.parametrize('centrics', ['all', 'none', 'some'])
+def test_RiceWoolfsonReferencePrior(mc_samples, centrics):
+    if centrics == 'all':
+        centric = np.ones(len(Fobs), dtype=bool)
+    elif centrics == 'none':
+        centric = np.zeros(len(Fobs), dtype=bool)
+    elif centrics == 'some':
+        centric = np.random.choice([True, False], len(Fobs))
+        centric[observed][0] = True
+        centric[observed][1] = False
+    else:
+        raise ValueError(f"received value, {centrics}, for parameter `centrics` which is not one of 'all', 'none', or 'some'")
+    p = RiceWoolfsonReferencePrior(Fobs[observed], SigFobs[observed], centric[observed], observed)
+    q = RiceWoolfson(Fobs, SigFobs, centric)
+    ReferencePrior_test(p, q, mc_samples)
+
