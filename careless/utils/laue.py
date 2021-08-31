e1ed68c87b5e046f5beef0538914781870bdb74e
Kevin Dalton <kevinmdalton@gmail.com>
2021-08-31 13:34:47 -0400

    cheaper dHKL calculation

--- careless/utils/laue.py
+++ careless/utils/laue.py
@@ -79,6 +79,7 @@ def expand_harmonics(ds, dmin=None,  wavelength_key='Wavelength'):
     H = ds.loc[:,['H', 'K', 'L']].to_numpy(np.int32)
     H_0_asu = (H/np.gcd.reduce(H, axis=1)[:,None]).astype(np.int32)
     ds.loc[:,['H', 'K', 'L']] = n[:,None] * H_0_asu
+    ds['dHKL'] = ds['d_0'] / ds['harmonic'] 
 
     return ds
 
