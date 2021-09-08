909e2cd32aed938e3dc405c6946a4bf88a6a5b58
Kevin Dalton <kevinmdalton@gmail.com>
2021-09-08 18:04:30 -0400

    zoop version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='careless',
-    version='0.1.5',
+    version='0.2.0',
     author='Kevin M. Dalton',
     author_email='kmdalton@fas.harvard.edu',
     packages=find_packages(),
