6802841eabf413179e9dd6240ba52321b240b580
Kevin Dalton <kevinmdalton@gmail.com>
2021-09-09 11:50:25 -0400

    do cchalf by default

--- careless/args/crossvalidation.py
+++ careless/args/crossvalidation.py
@@ -1,8 +1,8 @@
 name = "Crossvalidation"
 description = """
 Careless supports two sorts of crossvalidation. The first is used to assess the validity of a particular merging
-strategy (see Model Selection). The second is meant to assess the quality of a data set (see Data Consistency).
-You can use either or both of these strategies during merging. \n
+strategy (see Model Selection). The second is meant to assess the quality of a data set (see Data Consistency). 
+By default, careless will run the latter. However, You can use any combination of these strategies using the provided flags. \n
 
   Model Selection\n
   ---------------\n
@@ -23,17 +23,18 @@ You can use either or both of these strategies during merging. \n
 
 args_and_kwargs = (
     (("--test-fraction", ),  {
-        "help" : "Output model predictions for a held-out fraction of data. This should be used for model selection purposes."
-                 "By default, no data will be held out during training. " ,
+        "help" : "Output model predictions for a held-out fraction of data. This should be used for model selection purposes. "
+                 "By default, no data will be held out during training, and predictions will not be written to file. " ,
         "type" : float,
         "default" : None,
     }),
 
     (("--merge-half-datasets", ),  {
-        "help":"After training, split the data in half randomly by image and merge each half using the scaling model learned on the training fraction."
-               "The output of each half will be written to a file which can be used to estimate traditional CChalf type measures. ",
-        "action" : "store_true",
-        "default" : False,
+        "help":"After training, split the data in half randomly by image and merge each half using the scaling model learned on the training fraction. "
+               "The output of each half will be written to a file which can be used to estimate traditional CChalf type measures. The full data set will"
+               " always be used to generate half data sets irrespective of the test fraction. This parameter defaults to True.",
+        "type" : bool,
+        "default" : True,
     }),
 )
 
