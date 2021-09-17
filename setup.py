c64fe4a9d488f05b9b86cf4af4d82f05a7b1ece4
Kevin Dalton <kevinmdalton@gmail.com>
2021-09-17 13:37:09 -0400

    cleanup & add metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 from setuptools import setup, find_packages
 
+
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/Hekstra-Lab/careless/issues",
+    "Source Code": "https://github.com/Hekstra-Lab/careless",
+}
+
+
+LONG_DESCRIPTION = """
+``careless`` is a command line utility for merging 
+data from x-ray crystallography experiments. It 
+can be used to simultaneously scale and merge integrated
+reflection intensities from conventional, laue, 
+and free-electron laser experiments. It uses approximate 
+Bayesian inference and deep learning in to estimate
+merged structure factor amplitudes under a Wilson prior.
+"""
+
 setup(
     name='careless',
     version='0.1.6',
     author='Kevin M. Dalton',
     author_email='kmdalton@fas.harvard.edu',
+    license="MIT",
     packages=find_packages(),
-    description='Scaling and merging crystallographic data with TensorFlow and Variational Inference',
+    long_description=LONG_DESCRIPTION,
+    description='Merging crystallography data without much physics.',
+    project_urls=PROJECT_URLS,
+    python_requires=">=3.7,<=3.9",
+    url="https://github.com/Hekstra-Lab/careless",
     install_requires=[
         "reciprocalspaceship>=0.9.8,<=0.9.15",
         "tqdm",
@@ -19,7 +41,6 @@ setup(
             'scripts/ccplot',
             'scripts/ccanom_plot',
             'scripts/make_difference_map',
-            'scripts/optimistic_uncertainties',
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-cov', 'pytest-xdist'],
