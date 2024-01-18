486136a43a985b65be794125d72b3c4086311b43
Kevin Dalton <kmdalton@pop-os.localdomain>
2024-01-18 12:18:21 -0500

    pin tf version to avoid tfp error

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         "reciprocalspaceship>=0.9.16",
         "tqdm",
-        "tensorflow>=2.8",
+        "tensorflow<2.16",
         "tensorflow-probability",
         "matplotlib",
         "seaborn",
