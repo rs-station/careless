a5a6a14362bb7b737f9d98630157ec1edce2ab0d
Kevin Dalton <kmdalton@slac.stanford.edu>
2024-11-12 05:09:47 -0500

    remove pkg_resources dep

--- careless/__init__.py
+++ careless/__init__.py
@@ -1,9 +1,15 @@
 # Version number for careless
 def getVersionNumber():
-    import pkg_resources
+    version = None
+    try:
+        from setuptools.version import metadata
 
-    version = pkg_resources.require("careless")[0].version
+        version = metadata.version("careless")
+    except ImportError:
+        from setuptools.version import pkg_resources
+
+        version = pkg_resources.require("careless")[0].version
+
     return version
-
 
 __version__ = getVersionNumber()
